9876a576741a224a6669a25254c80b1c47fa0615
morispolanco <morispolanco@gmail.com>
2023-07-26 15:15:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def send_data(user, message):
         "query": message,
         "response_mode": "streaming",
         "conversation_id"
-        "user": user
+        "user": mpolanco
     }
     response = requests.post(url, headers=headers, json=data)
     try:
