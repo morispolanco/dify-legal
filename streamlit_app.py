ebb7a7779ac28ec4eb2836efa9c81470d83cdfb6
morispolanco <morispolanco@gmail.com>
2023-07-26 07:14:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,37 +2,28 @@ import requests
 import streamlit as st
 import json
 
-# Streamlit Configuration
-st.set_page_config(page_title='Chat Application')
+def send_chat_message(query, conversation_id, user):
+    url = 'https://api.dify.ai/v1/chat-messages'
+    headers = {
+        'Authorization': 'Bearer app-cGt4ZVQJ94qGNMthIXMPQj1l',
+        'Content-Type': 'application/json',
+    }
+    payload = {
+        "inputs": {},
+        "query": query,
+        "response_mode": "streaming",
+        "conversation_id": conversation_id,
+        "user": user
+    }
 
-# Variables
-secret_key = "app-cGt4ZVQJ94qGNMthIXMPQj1l"  # Replace with your actual key
-url = 'https://api.dify.ai/v1/chat-messages'
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
 
-# Headers
-headers = {
-    'Authorization': 'Bearer '+secret_key,
-    'Content-Type': 'application/json'
-}
- 
-# Input Field
-user_input = st.text_input("Say something")
+    return response.json()
 
-if st.button('Send'):
-    if user_input:
+# Example usage
+query = "es-ES"
+user = "Moris"
 
-        # Data
-        data = json.dumps({
-            "inputs": {},
-            "query": user_input,
-            "response_mode": "streaming",
-            "user": "abc-123",
-                    })
-
-        # Request
-        response = requests.request("POST", url, headers=headers, data=data)
-
-        # Response
-        st.write(response.json())
-    else:
-        st.write('Input field is empty')
+response = send_chat_message(query, conversation_id, user)
+print(response)
