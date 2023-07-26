7e5bd71819c11312e5526ce97b6e4c70266782e4
morispolanco <morispolanco@gmail.com>
2023-07-26 06:34:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ if st.button('Send'):
             "inputs": {},
             "query": user_input,
             "response_mode": "streaming",
-            "conversation_id": "",
+            "conversation_id": " ",
             "user": "abc-123"
         })
 
