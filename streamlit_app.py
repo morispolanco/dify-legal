f4fd0b96ab0616b621f104d59a476a89d4a10d21
morispolanco <morispolanco@gmail.com>
2023-07-27 06:19:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,14 @@ from datetime import datetime
 secret_key = 'app-OZw6qix4wsjQl4MUTmlpEukZ'  # Reemplace con su clave secreta
 
 def send_data(user, message):
-    url = 'https://api.dify.ai/v1'
+    url = 'https://api.dify.ai/v1/chat-messages'
     headers = {
         'Authorization': f'Bearer {secret_key}',
         'Content-Type': 'application/json'
     }
     data = {
         "inputs": {},
-        "query": message,
+        "query": "message",
         "response_mode": "streaming",
         "conversation_id": "",  # Reemplace con el ID de la conversación que desea utilizar
         "user": ""
