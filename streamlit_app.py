74a8e98142e658cb981b9d63ccfcca6daf127aab
morispolanco <morispolanco@gmail.com>
2023-07-27 07:29:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
-# Import necessary libraries
+# Importing necessary libraries
 import streamlit as st
 import requests
 import json
 
-# API configuration
+# API configuration, should be secured
 API_ENDPOINT = "https://api.dify.ai/v1/chat-messages"
 HEADERS = {
     'Content-Type': 'application/json',
@@ -13,8 +13,14 @@ HEADERS = {
 # Function to send a message to the API
 def send_message(message):
     data = {"message": message}
-    response = requests.post(API_ENDPOINT, headers=HEADERS, data=json.dumps(data))
-    return response.json()
+    try:
+        response = requests.post(API_ENDPOINT, headers=HEADERS, data=json.dumps(data))
+        response.raise_for_status()  # raise exception if status is not 200
+    except requests.exceptions.RequestException as err:
+        st.write('Whoops, something went wrong:', err)
+        return False
+    else:
+        return response.json()
 
 # Title
 st.title("Chatbot")
@@ -25,5 +31,6 @@ user_input = st.text_input("Enter a message")
 # When the user clicks the 'Send' button,
 # the message is sent to the API and the response is displayed.
 if st.button('Send'):
-    response = send_message(message=user_input)
-    st.write('Chatbot response:', response.get('response', 'No response available'))
+    response = send_message(user_input)
+    if response:
+        st.write('Chatbot response:', response.get('response', 'No response available'))
