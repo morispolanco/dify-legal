04eb34444132557ef1ecba71664003cd65c812b3
morispolanco <morispolanco@gmail.com>
2023-07-27 07:22:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,18 @@ import json
 API_ENDPOINT = "https://api.dify.ai/v1/chat-messages"
 HEADERS = {
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer app-OZw6qix4wsjQl4MUTmlpEukZ'
+    'Authorization': 'app-OZw6qix4wsjQl4MUTmlpEukZ'
 }
 
 # Function to send a message to the API
-def send_message(message):
-    data = {"message": message}
+def send_message(message, query, conversation_id, user):
+    data = {
+        "inputs": {},
+        "query": query,
+        "response_mode": "streaming",
+        "conversation_id": conversation_id,
+        "user": user
+    }
     response = requests.post(API_ENDPOINT, headers=HEADERS, data=json.dumps(data))
     return response.json()
 
@@ -21,9 +27,12 @@ st.title("Chatbot")
 
 # User input
 user_input = st.text_input("Enter a message")
+query_input = st.text_input("Enter a query")
+conversation_id_input = st.text_input("Enter a conversation ID")
+user_input_id = st.text_input("Enter a User ID")
 
 # When the user clicks the 'Send' button,
 # the message is sent to the API and the response is displayed.
 if st.button('Send'):
-    response = send_message(message=user_input)
+    response = send_message(message=user_input, query=query_input, conversation_id=conversation_id_input, user=user_input_id)
     st.write('Chatbot response:', response.get('response', 'No response available'))
